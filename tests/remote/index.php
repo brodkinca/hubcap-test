8816fdfe0c982a4a63cc1a03d7612a7814257ab8
Ryan Brodkin <ryan@brodkinca.com>
2012-10-24 09:49:01 -0700

    Adding remote test helpers. Adding Pagoda Box support.

---
+++ tests/remote/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Remote Data Return
+ *
+ * PHP Version 5.3
+ *
+ * Prints data used by CURL library integration tests.
+ *
+ * @category  Library
+ * @package   BCA/CURL
+ * @author    Brodkin CyberArts <support@brodkinca.com>
+ * @copyright 2012 Brodkin CyberArts.
+ * @license   All rights reserved.
+ * @version   GIT: $Id$
+ * @link      https://github.com/brodkinca/BCA-PHP-CURL
+ */
+
+header('Content-Type: application/json');
+
+// Set a constant value against which we can test
+$data['foo'] = 'bar';
+
+// Fill in parameters
+parse_str(file_get_contents("php://input"), $params);
+
+$data['_GET'] = $_GET;
+$data['_POST'] = $_POST;
+$data['_PUT'] = $params;
+$data['_DELETE'] = $params;
+
+// Return all server data
+$data['_SERVER'] = $_SERVER;
+
+// Respond to request with JSON
+echo json_encode($data);
